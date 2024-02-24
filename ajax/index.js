c2dbc961c9750dc0d18a312faee6f50ff2f5705f
Bashar Sheikh <122031915+Basher009@users.noreply.github.com>
2024-02-24 16:27:13 +0600

    add ajax
    hello

---
+++ ajax/index.js
@@ -0,0 +1,23 @@
+
+// function loadDoc(){
+    // const xhtpp = new XMLHttpRequest()
+
+    // xhtpp.onload = function(){
+    //     document.getElementById("demo").innerHTML= this.response
+
+    // }
+
+    // xhtpp.open('GET','data/data.php');
+    // xhtpp.send();
+
+
+   
+
+
+
+// }
+
+$('p').css({'color':'red','font-size':'3rem','font-style':'italic'})
+
+
+ 
